1a52df9deaed8fa6ddee34d6c80b60b805246b7e
Farris <jfarris@gpstrategies.com>
2018-05-21 10:17:00 -0400

    merge Restructure branch

---
+++ Gruntfile.js
@@ -0,0 +1,113 @@
+module.exports = function(grunt){
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    useminPrepare: {
+      html: 'app/**/*.html',
+      options: {
+        dest: 'dist'
+      }
+    },
+
+    usemin:{
+      html:['dist/**/*.html']
+    },
+
+    copy: {
+      build: {
+        cwd: 'app',
+        src: ['**', '!**/*.scss', '!**/vendors/**',
+        '!**/base/**', '!**/component/**', '!**/layout/**', '!**/components/*.js', '!**src/js/*.js', '!**/api/**'],
+        dest: 'dist',
+        expand: true
+      }
+    },
+
+    clean: {
+      build: {
+        src: [ 'dist' ]
+      },
+    },
+
+    sass: {
+      dist: {
+        files: {
+          //Default Sass to CSS
+          'app/dir/css/default/main.css': 'app/dir/css/default/style.scss',
+          //GM Selling Skills Sass to CSS
+          'app/dir/css/themes/gm_selling_skills/theme.css': 'app/dir/css/default/themes/gm_selling_skills/style.scss'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      }
+    },
+
+    cssmin: {
+      build: {
+        files: {
+          'dist/dir/css/default/main.css': [ 'dist/dir/css/default/main.css' ],
+          'dist/dir/css/themes/gm_selling_skills/theme.css': [ 'dist/dir/css/themes/gm_selling_skills/theme.css' ],
+          'dist/src/vendors/vendors.min.css': ['app/src/vendors/**/*.min.css'],
+        }
+      }
+    },
+
+    uglify: {
+      build: {
+        options: {
+          mangle: false
+        },
+        files: {
+          'dist/src/components/components.js': [ 'app/src/components/*.js' ],
+          'dist/src/js/functions.js': [ 'app/src/js/*.js' ],
+          'dist/src/js/api/api.js': [ 'app/src/js/api/*.js' ],
+          'dist/src/vendors/vendors.min.js': ['app/src/vendors/jquery/jquery-3.2.1.min.js', 'app/src/vendors/bootstrap/js/bootstrap.min.js', 'app/src/vendors/**/*.min.js']
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['app/src/vendors/jquery/jquery-3.2.1.min.js', 'app/src/vendors/**/*.min.js'],
+        dest: 'dist/src/vendors/vendors.min.js',
+      },
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: 'dist',
+          keepalive: true
+        }
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-usemin');
+
+  grunt.registerTask('dev', ['watch']);
+  grunt.registerTask(
+    'build',
+    'Compiles all of the assets and copies the files to the build directory.',
+    [ 'clean', 'copy', 'uglify', 'cssmin', 'useminPrepare', 'usemin', 'connect']
+  );
+}
---
+++ app/src/components/modal_functions.js
@@ -0,0 +1,108 @@
+function openModal(modalType, assessmentID, clickTarget) {
+  var courseData = JSON.parse(localStorage.getItem(LOCAL_COURSE_DATA_ID));
+  var pathStr = courseData.THEME_PATH;
+  switch(modalType) {
+    case 'splashPage':
+      $('#modalContainer').html(getSplashPage());
+      break;
+    case 'glossary':
+      $('#modalContainer').html(getGlossary());
+      glossaryNavigate();
+
+      break;
+    case 'help':
+      $('#modalContainer').html(getHelpPage());
+      break;
+    case 'assessment':
+      $('#modalContainer').html(getAssessment());
+      launchAssessment(assessmentID, clickTarget);
+      break;
+  }
+
+  $('.modal').on('shown.bs.modal', function (e) {
+    $('.modal .modal-body').css({height: $('.modal').height()});
+  });
+
+  $('.modal').on('hidden.bs.modal', function (e) {
+    $('#modalContainer').html('');
+  });
+
+  $('.modal').modal();
+}
+
+function openContentModal(heading, content) {
+  $('#modalContainer').html('<div class="modal fade" id="contentModal" tabindex="-1" role="dialog" aria-labelledby="contentModalLabel"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img src="'+pathStr+'/resouces/img/btn_close.png" alt="close the modal"></span></button><h4 class="modal-title" id="">'+heading+'</h4></div><div class="modal-body clearfix">'+content+'</div></div></div></div>');
+
+  $('.modal').on('hidden.bs.modal', function (e) {
+    $('#modalContainer').html('');
+  });
+
+  $('.modal').modal();
+}
+
+function openVidModal(heading,vidSrc,posterSrc) {
+  $('#modalContainer').html("<div class='modal fade' id='vidModal' tabindex='-1' role='dialog' aria-labelledby='vidModalLabel'><div class='modal-dialog' role='document'><div class='modal-content'><div class='modal-header'><button type='button' class='close' data-dismiss='modal' aria-label='Close'><span aria-hidden='true'><img src='"+pathStr+"/media/img/btn_close.png' alt='close the modal'></span></button><h4 class='modal-title' id=''>"+heading+"</h4></div><div class='modal-body clearfix'><video id='' src='"+vidSrc+"' preload='none' controls poster='"+posterSrc+"'>Sorry, your browser doesn't support embedded videos, but don't worry, you can <a href=''>download it</a> and watch it with your favorite video player!</video></div></div></div></div>");
+
+  $('.modal').on('hidden.bs.modal', function (e) {
+    $('#modalContainer').html('');
+  });
+
+  $('.modal').modal();
+}
+
+function getSplashPage() {
+  var html = '<div class="modal" id="splashPageModal" tabindex="-1" role="dialog" aria-labelledby="splashPageModalLabel"><div class="modal-dialog" role="document" style="max-width: 1000px"><div class="modal-content"><div class="row"><div class="col-md-12"><img class="img-fluid mx-auto d-block" data-dismiss="modal" src="dir/media/img/assets/00_splash_lg.jpg" alt="splash page image"><img class="img-fluid mx-auto d-none" data-dismiss="modal" src="dir/media/img/assets/00_splash_sm.jpg" alt="splash page image"></div></div></div></div></div><audio autoplay><source src="../../dir/media/audio/0.mp3"/></audio>';
+  return html;
+}
+
+/* NEW GLOSSARY METHOD USING XML */
+
+function getGlossary() {
+  var glossary = '<div class="modal fade" id="glossaryModal" tabindex="-1" role="dialog" aria-labelledby="glossaryModalLabel"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><div class="modal-title-wrapper"><h4 class="modal-title" id="glossaryModalLabel">GLOSSARY</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img src="dir/media/img/btn_close_glossary.png" alt="close the glossary"></span></button></div><div class="alphabet"><a id="" href="#">a</a><a id="" href="#">b</a><a id="" href="#">c</a><a id="" href="#">d</a><a id="" href="#">e</a><a id="" href="#">f</a><a id="" href="#">g</a><a id="" href="#">h</a><a id="" href="#">i</a><a id="" href="#">j</a><a id="" href="#">k</a><a id="" href="#">l</a><a id="" href="#">m</a><a id="" href="#">n</a><a id="" href="#">o</a><a id="" href="#">p</a><a id="" href="#">q</a><a id="" href="#">r</a><a id="" href="#">s</a><a id="" href="#">t</a><a id="" href="#">u</a><a id="" href="#">v</a><a id="" href="#">w</a><a id="" href="#">x</a><a id="" href="#">y</a><a id="" href="#">z</a></div></div><div class="modal-body">';
+
+
+  for(i = 0; i < courseData.glossary.items.length; i++){
+
+    var term = courseData.glossary.items[i].term;
+    var def = courseData.glossary.items[i].definition;
+    var glossID = term.charAt(0);
+
+
+    var glossItem = '<div class="gloss-item" id="'+glossID+'"><h5>'+term+'</h5><p>'+ def +'</p></div>'
+
+    glossary += glossItem;
+  }
+
+  glossary += '</div></div></div></div>';
+
+  return glossary;
+
+}
+
+function glossaryNavigate(){
+  $(".alphabet a").click(function() {
+    var c = '#' + $(this).text()
+
+    if($(c).offset().top > 140 || 120 > $(c).offset().top){
+      $('.modal-body').scrollTop(
+        0
+      );
+
+      $('.modal-body').animate({
+        scrollTop: $(c).offset().top - 130
+      });
+    }
+  });
+}
+
+/* end NEW GLOSSARY METHOD USING XML */
+
+function getHelpPage() {
+  var html = '<div class="modal fade" id="helpModal" tabindex="-1" role="dialog" aria-labelledby=""><div class="modal-dialog" style="max-width:none" role="document"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="helpModalLabel">HELP</h4><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true"><img src="dir/media/img/btn_close_help.png" alt="close the help page"></span></button></div><div class="modal-body"><div class="help-section"><h5>NAVIGATION</h5><div class="help-item"><h6>TABLE OF CONTENTS</h6><img src="dir/media/img/help_toc.png" alt="t.o.c. icon"/><p>Use this to navigate to different pages in the course.</p></div><div class="help-item"><h6>CONTENT AREA</h6><p>Content will appear here. Follow the on-screen instructions for videos or interactives. Some pages may scroll.</p></div><div class="help-item"><h6>PAGE COUNT</h6><img src="dir/media/img/help_page_count.png" alt="page count screen shot"/><p>Check your progress through the course.</p></div><div class="help-item"><h6>BACK/NEXT BUTTONS</h6><img src="dir/media/img/help_back_next.png" alt="page count screen shot"/><p>Use these buttons to move forward or backward in the course.</p></div></div><div class="help-section"><h5>HELP</h5><div class="help-item"><h6>HELP</h6><img src="dir/media/img/help_help.png" alt="page count screen shot"/><p>This page describes how to use the course.</p></div></div></div></div></div></div>';
+  return html;
+}
+
+function getAssessment() {
+  var html = '<div class="modal" id="assessmentModal" tabindex="-1" role="dialog" aria-labelledby="assessmentModalLabel" data-backdrop="static"><div class="modal-dialog" role="document" style="max-width: 1000px"><div class="modal-content"><div class="row"><div class="col-md-12 assessment-container"></div></div></div></div></div>';
+  return html;
+}
